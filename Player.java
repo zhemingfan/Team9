827cd43579726d8a633f696af6ce675cbba837ca
The-Real-Guy <aasdimaranan@gmail.com>
2019-07-17 14:28:49 -0600

    Added all required instance variables, constructor, and setter methods

--- Player.java
+++ Player.java
@@ -1,3 +1,27 @@
-class Player{
+public class Player{
+  private int playerHealth;
+  private int money;
+  private int xcoord;
+  private int ycoord;
+  //private Point checkpoint;
 
+  public Player(int aHealth, int initCurrency, int xPos, int yPos){
+    this.setHealth(aHealth);
+    this.setMoney(initCurrency);
+    this.setPosition(xPos, yPos);
+  }
+
+  public void setHealth(int initHealth){
+    playerHealth = initHealth;
+  }
+
+  public void setMoney(int initMoney){
+    money = initMoney;
+  }
+
+  public void setPosition(int xPos, int yPos){
+    xcoord = xPos;
+    ycoord = yPos;
+    //checkpoint = new Point(xPos, yPos);
+  }
 }

68022fee5cb6fce5ad06ae188f66a48b5fd17ebd
klinh <plekhanhlinh12345@gmail.com>
2019-07-16 21:13:15 -0600

    test

---
+++ src/Towers.java
@@ -0,0 +1,16 @@
+
+public class Towers {
+	private int xCoor, yCoor;
+	private int range;
+	private int damage;
+	
+	public Towers(int X, int Y, int aRange, int aDamage) {
+		xCoor = X;
+		yCoor = Y;
+		range = aRange;
+		damage = aDamage;
+	}
+	
+	
+
+}

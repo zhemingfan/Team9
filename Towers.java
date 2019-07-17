888ce916cb023c331105794fcdd31f4b5e9acb12
Linh Phan <40071008+plklinh@users.noreply.github.com>
2019-07-16 22:10:13 -0600

    Add files via upload

---
+++ Towers.java
@@ -0,0 +1,56 @@
+
+public class Towers {
+	private int xCoor, yCoor;
+	private int range;
+	private int damage;
+	
+	public int getxCoor() {
+		return xCoor;
+	}
+
+	public void setxCoor(int xCoor) {
+		this.xCoor = xCoor;
+	}
+
+	public int getyCoor() {
+		return yCoor;
+	}
+
+	public void setyCoor(int yCoor) {
+		this.yCoor = yCoor;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public Towers(int X, int Y, int aRange, int aDamage) {
+		xCoor = X;
+		yCoor = Y;
+		range = aRange;
+		damage = aDamage;
+	}
+	
+	private double getDistancefromEnemy(Enemy anEnemy) {
+		int otherX = anEnemy.getXCoor();
+		int otherY = anEnemy.getYCoor();
+		double deltaXsquared = Math.pow((double)(xCoor - otherX), 2.0);
+		double deltaYsquared = Math.pow((double)(yCoor - otherY), 2.0);
+		return Math.sqrt(deltaXsquared + deltaYsquared);
+	}
+	
+	public void attack() {
+		
+	}
+	
+
+}

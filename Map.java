0e84de0fa00800f11c9f62dc997a2861ee38be12
Jonathan <jawnscott123@gmail.com>
2019-07-18 14:06:14 -0600

    Updated Files

--- Map.java
+++ Map.java
@@ -1,31 +1,28 @@
-public class Map{
+public class Map extends Game{
+
   
   public Map() {
   }
-  public void createPath(String[][] grid){
-    for(int r = 0; r < grid.length; r++) {
-      for(int c = 0; c < grid[0].length; c++) {
-        if (grid[c][r].contentEquals("0")){
-          grid[c][r] = " ";
-        }
-        grid[c][2] = "-";
-      }
-  }
-  }
+
   public void generateGrid(String[][] grid) {
     for(int r = 0; r < grid.length; r++) {
       for(int c = 0; c < grid[0].length; c++) {
         grid[c][r] = "0";
+        grid[c][2] = "-";
       }
     }
   }
-  
+
   public void printGrid(String[][] grid) {
     for (int r = 0; r < grid.length; r++){
       System.out.println("");
-      for (int c = 0; c < grid[0].length; c++){
+      for (int c = 0; c < grid[r].length; c++){
        System.out.print(grid[c][r]+" ");
        }
-      } 
-   }
+      }
+    System.out.println("\n");
+    }
   }
+
+
+  
